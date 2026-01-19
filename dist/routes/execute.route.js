be75b83ce323a16103ceef896899268b99f4d1e2
YasirAhmed2 <yasirahmed5077@gmail.com>
2026-01-19 22:14:04 +0500

    fix: configure gitignore and remove node_modules from tracking

--- dist/routes/execute.route.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { runCode } from "../controllers/codeExecution.controller.js";
-const executeRouter = Router();
-executeRouter.post("/", runCode);
-export default executeRouter;
--- dist/utils/jwt.utils.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-import "dotenv/config";
-const PRIVATE_KEY = process.env.JWT_SECRET;
-export const generateToken = (payload, expiresIn = "15m") => {
-    return jwt.sign(payload, PRIVATE_KEY, { expiresIn });
-};
-export const verifyToken = (token) => {
-    return jwt.verify(token, PRIVATE_KEY);
-};
